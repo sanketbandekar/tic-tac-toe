8b9a704ee876cdd48169cd9c88de408b6191a2fc
sanket <bandekarsanket03@gmail.com>
2021-05-21 21:04:57 +0530

    added styles (sass)

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Square = (props) => {
     console.log(props);
     return (
-        <button type="button">{props.value}</button>
+        <button type="button" className="square">{props.value}</button>
     )
 }
 
