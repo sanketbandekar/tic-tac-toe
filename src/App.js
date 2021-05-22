b7dfb05480ab313876a46008d734d26672694e1e
sanket <bandekarsanket03@gmail.com>
2021-05-22 19:47:59 +0530

    displayed history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Board from "./components/Board";
+import History from "./components/History";
 import { calculateWinner } from "./helper";
 import "./styles/root.scss";
 
@@ -9,8 +10,6 @@ const App = () => {
   const [currentMove, setCurrentMove] = useState(0);
 
   const current  = history[currentMove];
-
-  console.log('history:', history);
 
     const winner = calculateWinner(current.board);
 
@@ -42,11 +41,16 @@ const App = () => {
         setCurrentMove(prev => prev + 1);
     };
 
+    const moveTo = (move) =>{
+      setCurrentMove(move);
+    }
+
   return(
     <div className="app">
     <h1>TIC TAC TOE</h1>
     <h2>{message}</h2>
     <Board board = {current.board} handleClickSquare={handleClickSquare}/>
+    <History history = {history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 };
